e05a89fc46f42449e981384bc13a19a1d620c791
chengengwei <1213567406@qq.com>
2022-04-07 16:28:30 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" />
+    </van-cell-group>
   </div>
 </template>
 
